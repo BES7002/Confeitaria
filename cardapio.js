55dc81470e1f76ecd47e0a9ff5730b69fcc04533
BES7002 <135378263+BES7002@users.noreply.github.com>
2025-11-26 20:14:29 -0300

    Criando tags dinamicamentes.

--- cardapio.js
+++ cardapio.js
@@ -1,2 +1,30 @@
 alert('oi foi carregado')
 
+
+
+function criarItemCardapio(titulo, descricao, foto) {
+    const divItemCardapio = document.createElement('div');
+    divItemCardapio.className = 'item-cardapio';
+
+    const h3Titulo = document.createElement('h3');
+    h3Titulo.textContent = titulo;
+
+    const pDescricao = document.createElement('p');
+    pDescricao.className = 'descricao';
+    pDescricao.textContent = descricao;
+
+    const img = document.createElement('img');
+    img.src = foto;
+    img.className = 'img-item';
+
+    const divC = document.getElementById('cardapio')
+
+    divItemCardapio.appendChild(h3Titulo)
+    divItemCardapio.appendChild(pDescricao)
+    divItemCardapio.appendChild(img)
+
+    divC.appendChild(divItemCardapio);
+}
+
+criarItemCardapio('Bolo de chocolate', 'Um classico irresistível com camadas de chocolate.', 'https://images.food52.com/hCMCcgnrNaS51pbWEJUbEXkrLVo=/60a6e4b8-2765-4480-88f3-f96f73107941--2022-0607_angel-food-cake-with-strawberries-and-whipped-cream_seo_3x2_mj-kroeger-067.jpg');
+
